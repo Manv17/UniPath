c8ad9668ba23e4540c88beea5851ff844f19bc9a
Manv17 <singhmanveer1106@gmail.com>
2026-09-05 18:12:12 +0200

    initial commit

---
+++ UniPath/UniPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniPathApp.swift
+//  UniPath
+//
+//  Created by Manveer Singh on 05/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct UniPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
