252610e9ff49794efba83a5f4971ce7dc3f87de0
Manv17 <singhmanveer1106@gmail.com>
2026-09-12 17:09:13 +0200

    test: add career statistics tests

---
+++ UniPathTests/UniPathTests.swift
@@ -0,0 +1,16 @@
+//
+//  UniPathTests.swift
+//  UniPathTests
+//
+//  Created by Manveer Singh on 12/09/2026.
+//
+
+import Testing
+
+struct UniPathTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
