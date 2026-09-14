4bd4ff119479c6535c3d60f840a7e22e369cf644
Manv17 <singhmanveer1106@gmail.com>
2026-09-14 18:43:19 +0200

    feat: add logo

---
+++ UniPath/Views/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  UniPath
+//
+//  Created by Manveer Singh on 05/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    @AppStorage("hasSeenWelcome") private var hasSeenWelcome = false
+
+    @State private var career: Career?
+    @State private var hasStudentID = false
+
+    var body: some View {
+
+        if !hasSeenWelcome {
+
+            WelcomeView {
+                hasSeenWelcome = true
+            }
+
+        } else if career == nil {
+
+            CareerSetupView { newCareer in
+                career = newCareer
+            }
+
+        } else if !hasStudentID {
+
+            StudentIDSetupView {
+                hasStudentID = true
+            }
+
+        } else {
+
+            Text("UniPath")
+        }
+        .onAppear {
+            loadData()
+        }
+    }
+
+    private func loadData() {
+        career = CareerStorage.load()
+        hasStudentID = StudentIDStorage.load() != nil
+    }
+}
+
+
+
+#Preview {
+    ContentView()
+}
